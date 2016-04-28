558cd608ef96250bc974dcc1ab3b746c14d1e910
hujinyu <hujinyu@meituan.com>
2016-04-28 20:30:05 +0800

    通过aspectj完成controller接口拦截

---
+++ src/main/java/com/bigpotato/common/aspect/LogCommonAspect.java
@@ -0,0 +1,48 @@
+package com.bigpotato.common.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 通用日志拦截
+ * Created by hjy on 4/28/16.
+ */
+@Aspect
+@Component
+public class LogCommonAspect {
+    private final static Logger logger = LoggerFactory.getLogger(LogCommonAspect.class);
+
+    @Pointcut("execution(* com.bigpotato.controller.*.*(..))")
+    public void commonController() {
+    }
+
+    @Around("commonController()")
+    public void recordLog(ProceedingJoinPoint joinPoint) {
+        Object returnVal = null;
+        try {
+            returnVal = joinPoint.proceed();
+        } catch (Throwable throwable) {
+            logger.error("#LogCommonAspect recordLog#", throwable);
+        } finally {
+            HttpServletRequest req = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+
+            logger.warn("#LogCommonAspect recordLog#{} {}:request = {}, response = {}",
+                    joinPoint.getTarget().getClass().getSimpleName(),
+                    joinPoint.getSignature().getName(),
+                    req.getParameterMap(), returnVal);
+        }
+
+
+    }
+
+}
--- src/main/java/com/bigpotato/controller/FoodController.java
+++ src/main/java/com/bigpotato/controller/FoodController.java
@@ -1,12 +1,8 @@
 package com.bigpotato.controller;
 
-import com.bigpotato.common.log.LogCustomPointcut;
 import com.bigpotato.common.util.AppCommonUtil;
 import com.bigpotato.vo.AppFoodPageVo;
-import com.bigpotato.vo.AppPoiPageVo;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,13 +13,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/food")
 public class FoodController {
-	@Autowired
-	private LogCustomPointcut logCustomPointcut;
-	@RequestMapping(method = RequestMethod.GET)
+
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
 	@ResponseBody
 	public Object getAppFoodPage() {
 		AppFoodPageVo appFoodPageVo = new AppFoodPageVo();
-		logCustomPointcut.insert();
+		return AppCommonUtil.constructResponse(0, "success", appFoodPageVo);
+	}
+
+	@RequestMapping(value = "/info", method = RequestMethod.GET)
+	@ResponseBody
+	public Object foodDetail() {
+		AppFoodPageVo appFoodPageVo = new AppFoodPageVo();
 		return AppCommonUtil.constructResponse(0, "success", appFoodPageVo);
 	}
 }
